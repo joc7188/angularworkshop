f2229da0f77423b2d3e3761515c956cadb5a55cc
joseantonioolveracanizares <joc7188@gmail.com>
2018-05-21 23:45:27 +0200

    Cardlist hardcoded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { CardListComponent } from './card-list/card-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CardListComponent
   ],
   imports: [
     BrowserModule
